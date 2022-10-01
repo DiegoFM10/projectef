054988480d416be3fe65bfa3f41943ea56dfcd9b
diegoFm10 <diego479@gmail.com>
2022-10-01 00:18:04 -0500

    Add Sql Server connection string configuration

--- Program.cs
+++ Program.cs
@@ -1,6 +1,21 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using projectef;
+
 var builder = WebApplication.CreateBuilder(args);
+
+// builder.Services.AddDbContext<TareasContext>(p => p.UseInMemoryDatabase("TareasDB"));
+builder.Services.AddSqlServer<TareasContext>(builder.Configuration.GetConnectionString("cnTareas"));
+
 var app = builder.Build();
 
 app.MapGet("/", () => "Hello World!");
+
+//Endpoint dbConnection
+app.MapGet("/dbconexion", async ([FromServices] TareasContext dbContext) => 
+{
+    dbContext.Database.EnsureCreated();
+    return Results.Ok("Base de datos en memoria: " + dbContext.Database.IsInMemory());
+});
 
 app.Run();
