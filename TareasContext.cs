74d20a33a3aed37213e851ddc034f84ea65a182e
diegoFm10 <diego479@gmail.com>
2022-09-30 23:16:16 -0500

    Add DbContext configuration

--- TareasContext.cs
+++ TareasContext.cs
@@ -7,5 +7,6 @@ public class TareasContext : DbContext
 {
     public DbSet<Categoria> Categorias { get; set; }
     public DbSet<Tarea> Tareas { get; set; }
-    
+
+    public TareasContext(DbContextOptions<TareasContext> options): base(options) { }
 }
